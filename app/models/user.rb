1e3ce2cc5bc263f6f55f80dfaa6ae2f20e051343
Dominic <dominicwong09@gmail.com>
2023-04-22 18:39:14 +1200

    user sign up completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, :uniqueness => true, :presence => true
     has_many :journals
     has_many :plants, :through => :journals
 end
