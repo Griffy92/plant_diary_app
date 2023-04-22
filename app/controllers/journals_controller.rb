4636168cd1b47bb351cd7c7888086d665d703b67
Dominic <dominicwong09@gmail.com>
2023-04-22 23:50:51 +1200

    journal, new/create completed

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -8,4 +8,19 @@ class JournalsController < ApplicationController
     def show
         @journal = Journal.find params[:id]
     end
+
+    def new
+        @journal = @current_user.journals.new
+    end
+
+    def create
+        journal = @current_user.journals.create journal_params
+        redirect_to journal
+    end
+
+    private
+
+    def journal_params
+        params.require(:journal).permit(:name)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new user_params
 		if @user.save 
-			session[:user_id] = user.id
-			redirect_to root_path
+			session[:user_id] = @user.id
+			redirect_to journals_path
 		else
 			render :new
 		end
