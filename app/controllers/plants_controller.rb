495e41700d934da1688f2efb1a230d02f445127f
Dominic <dominicwong09@gmail.com>
2023-04-23 17:41:33 +1200

    updated plant show/index. Incomplete plant new

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -13,6 +13,23 @@ class PlantsController < ApplicationController
 	end
 
 	def new
+		@plant = @current_user.plants.new
+		@journal = @plant.journals.build
 	end
 
+	def create
+		@plant = @current_user.plants.build plant_params
+
+		if @plant.save 
+			redirect_to @plant
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def plant_params
+		params.require(:plant).permit(:name, :species, :last_watered, :cycle, :journals_attributes => :name)
+	end
 end
